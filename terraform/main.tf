5abf44bb74e29b2d0101574387f4b205546d2368
M. Kozlik <malkak@skyvar.co.il>
2023-12-24 13:53:01 +0200

    change step az login

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,10 @@
 terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.58.0"
+    }
+  }
   backend "azurerm" {
     resource_group_name      = "NetworkWatcherRG"
     storage_account_name     = "myfirsttrail"
