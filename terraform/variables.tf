6cd2a38e727dbdd23063213267ba70a9a2b98045
M. Kozlik <malkak@skyvar.co.il>
2023-12-26 08:57:39 +0200

    remove variables that not need

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -57,6 +57,41 @@ variable vnet_storage_account_name {
   default =  "myfirsttrail"
 }
 
+variable key_vault_name {
+  type        = string
+  default = "kv-mange-storages"
+}
+
+variable key_vault_sku_name {
+  type        = string
+  default     = "standard"
+}
+
+variable key_vault_certificate_permissions {
+  type        = list
+  default = ["Get", "List", "Update", "Create", "Import", "Delete", "Recover", "Backup", "Restore"]
+}
+
+variable key_vault_key_permissions {
+  type        = list
+  default = ["Create","Get"]
+}
+
+variable key_vault_secret_permissions {
+  type        = list
+  default = ["Get","Set","Delete","Purge","Recover"]
+}
+
+variable key_vault_storage_permissions {
+  type        = list
+  default =  ["Get", ]
+}
+
+variable key_vault_secret_name {
+  type        = string
+  default     = "CONNECTION-STRING"
+}
+
 variable app_service_plan_name{
   type = list(string)
   default = ["app-log-analytics","app-start-function","app-for-each-subscription","app-test-storages","app-end-function"]
@@ -114,70 +149,8 @@ variable FREQ_AUTOMATION_TEST_NUMBER {
   default = 1
 }
 
-variable ESSENTIAL_TAG {
-  type        = string
-  default = "essential"
-}
-
-variable ALERTS_DOCUMENTATION {
-  type        = string
-  default     = "alertsDocumentation"
-}
-
-variable DOCUMENTATION_TABLE {
-  type        = string
-  default     = "documentation"
-}
-
-variable DELETED_ACCOUNTS_TABLE {
-  type        = string
-  default     = "deletedStorages"
-}
-
-variable DOCUMENTATION_STORAGE_NAME {
-  type        = string
-  default     = "myfirsttrail"
-}
-
-
-
 variable logic_app_workflow_name {
   type        = string
   default = "logic-app-storage-management"
 }
 
-
-variable key_vault_name {
-  type        = string
-  default = "kv-mange-storages"
-}
-
-variable key_vault_sku_name {
-  type        = string
-  default     = "standard"
-}
-
-variable key_vault_certificate_permissions {
-  type        = list
-  default = ["Get", "List", "Update", "Create", "Import", "Delete", "Recover", "Backup", "Restore"]
-}
-
-variable key_vault_key_permissions {
-  type        = list
-  default = ["Create","Get"]
-}
-
-variable key_vault_secret_permissions {
-  type        = list
-  default = ["Get","Set","Delete","Purge","Recover"]
-}
-
-variable key_vault_storage_permissions {
-  type        = list
-  default =  ["Get", ]
-}
-
-variable key_vault_secret_name {
-  type        = string
-  default     = "CONNECTION-STRING"
-}
