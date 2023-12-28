e5d7f964ad0fcdab0f9b1812848c740a899b8baf
M. Kozlik <malkak@skyvar.co.il>
2023-12-28 14:20:21 +0200

    add key vault

--- function_apps/func-end-function/config_variables.py
+++ function_apps/func-end-function/config_variables.py
@@ -1,9 +1,14 @@
-import os
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
-connection_string=os.getenv("CONNECTION_STRING")
+KVUri=os.getenv("KVUri")
+credential = DefaultAzureCredential()
+client = SecretClient(vault_url=KVUri, credential=credential)
+connection_string = client.get_secret("CONNECTION-STRING").value
 excel_connection_string=os.getenv('EXCEL_CONNECTION_STRING')
 http_trigger_url = os.getenv("HTTP_TRIGGER_URL")
 deleted_accounts_table=os.getenv("DELETED_ACCOUNTS_TABLE")
--- function_apps/func-start-function/config_variables.py
+++ function_apps/func-start-function/config_variables.py
@@ -1,8 +1,13 @@
-import os
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
-connection_string=os.getenv("CONNECTION_STRING")
+KVUri=os.getenv("KVUri")
+credential = DefaultAzureCredential()
+client = SecretClient(vault_url=KVUri, credential=credential)
+connection_string = client.get_secret("CONNECTION-STRING").value
 
 documentation_table=os.getenv("DOCUMENTATION_TABLE")
