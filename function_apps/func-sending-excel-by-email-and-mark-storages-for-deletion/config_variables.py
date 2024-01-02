5bc0a1acf54ab1215e2709185fcba7041747af42
M. Kozlik <malkak@skyvar.co.il>
2024-01-02 13:52:39 +0200

    change connection string to take from key vault

--- function_apps/func-sending-excel-by-email-and-mark-storages-for-deletion/config_variables.py
+++ function_apps/func-sending-excel-by-email-and-mark-storages-for-deletion/config_variables.py
@@ -9,7 +9,7 @@ keyvault_uri=os.getenv("KEYVAULT_URI")
 secret_name = os.getenv("SECRET")
 credential = DefaultAzureCredential()
 client = SecretClient(vault_url=keyvault_uri, credential=credential)
-connection_string = client.get_secret(secret_name)
+connection_string = client.get_secret(secret_name).value
 # connection_string = os.getenv("CONNECTION_STRING")
 
 
