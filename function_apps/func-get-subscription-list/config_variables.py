2c2892e781546f3e2fbfe969a6b0e3ba2f6d7d2b
M. Kozlik <malkak@skyvar.co.il>
2024-01-02 09:16:53 +0200

    try to give the connection string from environment variables

--- function_apps/func-get-subscription-list/config_variables.py
+++ function_apps/func-get-subscription-list/config_variables.py
@@ -5,10 +5,12 @@ import os
 
 load_dotenv()
 
-keyvault_uri=os.getenv("KEYVAULT_URI")
-secret_name = os.getenv("SECRET")
-credential = DefaultAzureCredential()
-client = SecretClient(vault_url=keyvault_uri, credential=credential)
-connection_string = client.get_secret(secret_name).value
+# keyvault_uri=os.getenv("KEYVAULT_URI")
+# secret_name = os.getenv("SECRET")
+# credential = DefaultAzureCredential()
+# client = SecretClient(vault_url=keyvault_uri, credential=credential)
+# connection_string = client.get_secret(secret_name).value
+
+connection_string = os.getenv("CONNECTION_STRING")
 
 documentation_table=os.getenv("DOCUMENTATION_TABLE")
