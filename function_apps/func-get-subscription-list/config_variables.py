0d393c7c9bc0023eb085952fcd6a5cf9477091ed
M. Kozlik <malkak@skyvar.co.il>
2024-01-02 12:27:05 +0200

    add

--- function_apps/func-get-subscription-list/config_variables.py
+++ function_apps/func-get-subscription-list/config_variables.py
@@ -8,7 +8,7 @@ load_dotenv()
 keyvault_uri=os.getenv("KEYVAULT_URI")
 secret_name = os.getenv("SECRET")
 credential = DefaultAzureCredential()
-# client = SecretClient(vault_url=keyvault_uri, credential=credential)
+client = SecretClient(vault_url=keyvault_uri, credential=credential)
 # connection_string = client.get_secret(secret_name).value
 
 connection_string = os.getenv("CONNECTION_STRING")
