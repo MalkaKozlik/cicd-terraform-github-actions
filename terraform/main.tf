21b62a662f0aaa1ed3fcada780b68091cc8f80a5
M. Kozlik <malkak@skyvar.co.il>
2024-01-04 15:08:15 +0200

    add

--- terraform/main.tf
+++ terraform/main.tf
@@ -236,7 +236,7 @@ resource "azurerm_logic_app_workflow" "logic_app_workflow" {
   name                = var.logic_app_workflow_name
   location            = data.azurerm_resource_group.vnet_resource_group.location
   resource_group_name = data.azurerm_resource_group.vnet_resource_group.name
-  parameters = {
+  parameters  {
     workflows_logic_app_name = "logic-app-storage-management"
     sites_func_get_last_fetch_time_for_each_storage_account_externalid = "/subscriptions/a173eef2-33d7-4d55-b0b5-18b271f8d42b/resourceGroups/NetworkWatcherRG/providers/Microsoft.Web/sites/func_get_last_fetch_time_for_each_storage_account"
     sites_func_get_subscription_list_externalid = azurerm_linux_function_app.linux_function_app[1].id
