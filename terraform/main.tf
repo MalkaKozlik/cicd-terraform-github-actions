d9b43e538489114c7524041b4b31b20716ba6813
M. Kozlik <malkak@skyvar.co.il>
2024-01-04 15:27:03 +0200

    remove workflow dispatch

--- terraform/main.tf
+++ terraform/main.tf
@@ -236,18 +236,18 @@ resource "azurerm_logic_app_workflow" "logic_app_workflow" {
   name                = var.logic_app_workflow_name
   location            = data.azurerm_resource_group.vnet_resource_group.location
   resource_group_name = data.azurerm_resource_group.vnet_resource_group.name
-  workflow_parameters = {
-    workflows_logic_app_name = "logic-app-storage-management"
-    sites_func_get_last_fetch_time_for_each_storage_account_externalid = "/subscriptions/a173eef2-33d7-4d55-b0b5-18b271f8d42b/resourceGroups/NetworkWatcherRG/providers/Microsoft.Web/sites/func_get_last_fetch_time_for_each_storage_account"
-    sites_func_get_subscription_list_externalid = "/subscriptions/a173eef2-33d7-4d55-b0b5-18b271f8d42b/resourceGroups/NetworkWatcherRG/providers/Microsoft.Web/sites/func_get_subscription_list"
-    sites_func_get_storage_list_by_subscription_externalid = "/subscriptions/a173eef2-33d7-4d55-b0b5-18b271f8d42b/resourceGroups/NetworkWatcherRG/providers/Microsoft.Web/sites/func_get_storage_list_by_subscription"
-    sites_func_test_storage_externalid = "/subscriptions/a173eef2-33d7-4d55-b0b5-18b271f8d42b/resourceGroups/NetworkWatcherRG/providers/Microsoft.Web/sites/func_test_storage"
-    sites_func_sending_excel_by_email_and_mark_storages_for_deletion_externalid = "/subscriptions/a173eef2-33d7-4d55-b0b5-18b271f8d42b/resourceGroups/NetworkWatcherRG/providers/Microsoft.Web/sites/func_sending_excel_by_email_and_mark_storages_for_deletion"
-    connections_office365_1_externalid = "/subscriptions/a173eef2-33d7-4d55-b0b5-18b271f8d42b/resourceGroups/NetworkWatcherRG/providers/Microsoft.Web/connections/office365-1"
-    location = "westeurope"
-    frequency = "Week"
-    interval = "1"
-  }
+  # workflow_parameters = {
+  #   workflows_logic_app_name = "logic-app-storage-management"
+  #   sites_func_get_last_fetch_time_for_each_storage_account_externalid = "/subscriptions/a173eef2-33d7-4d55-b0b5-18b271f8d42b/resourceGroups/NetworkWatcherRG/providers/Microsoft.Web/sites/func_get_last_fetch_time_for_each_storage_account"
+  #   sites_func_get_subscription_list_externalid = "/subscriptions/a173eef2-33d7-4d55-b0b5-18b271f8d42b/resourceGroups/NetworkWatcherRG/providers/Microsoft.Web/sites/func_get_subscription_list"
+  #   sites_func_get_storage_list_by_subscription_externalid = "/subscriptions/a173eef2-33d7-4d55-b0b5-18b271f8d42b/resourceGroups/NetworkWatcherRG/providers/Microsoft.Web/sites/func_get_storage_list_by_subscription"
+  #   sites_func_test_storage_externalid = "/subscriptions/a173eef2-33d7-4d55-b0b5-18b271f8d42b/resourceGroups/NetworkWatcherRG/providers/Microsoft.Web/sites/func_test_storage"
+  #   sites_func_sending_excel_by_email_and_mark_storages_for_deletion_externalid = "/subscriptions/a173eef2-33d7-4d55-b0b5-18b271f8d42b/resourceGroups/NetworkWatcherRG/providers/Microsoft.Web/sites/func_sending_excel_by_email_and_mark_storages_for_deletion"
+  #   connections_office365_1_externalid = "/subscriptions/a173eef2-33d7-4d55-b0b5-18b271f8d42b/resourceGroups/NetworkWatcherRG/providers/Microsoft.Web/connections/office365-1"
+  #   location = "westeurope"
+  #   frequency = "Week"
+  #   interval = "1"
+  # }
   workflow_parameters = {
     "workflows_logic_app_name": {
       "type": "string",
