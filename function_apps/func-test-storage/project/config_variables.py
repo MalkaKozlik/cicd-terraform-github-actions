d807897230855a41926db787189311d5a8990a93
M. Kozlik <malkak@skyvar.co.il>
2024-01-07 16:04:35 +0200

    remove value

--- function_apps/func-test-storage/project/config_variables.py
+++ function_apps/func-test-storage/project/config_variables.py
@@ -9,7 +9,7 @@ keyvault_uri=os.getenv("KEYVAULT_URI")
 secret_name = os.getenv("SECRET")
 credential = DefaultAzureCredential()
 client = SecretClient(vault_url=keyvault_uri, credential=credential)
-secret = client.get_secret(secret_name).value
+secret = client.get_secret(secret_name)
 connection_string = secret.value
 # connection_string = os.getenv("CONNECTION_STRING")
 
