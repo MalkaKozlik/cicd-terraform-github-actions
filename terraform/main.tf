d60ee551333ba8c2849c4a0e0d158ef6f24b3ad6
M. Kozlik <malkak@skyvar.co.il>
2024-01-07 09:49:37 +0200

    add

--- terraform/main.tf
+++ terraform/main.tf
@@ -254,10 +254,7 @@ resource "azurerm_logic_app_workflow" "logic_app_workflow" {
   #   interval = "1"
   # }
   workflow_parameters = {
-    "workflows_logic_app_name" : "{ \\
-      \"defaultValue\":\"logic-app-storage-management\", \\
-      \"type\" : \"string\" \\
-    }"
+    "workflows_logic_app_name" : "{ \"defaultValue\":\"logic-app-storage-management\", \"type\" : \"string\" \\}"
       
     #   defaultValue= "logic-app-storage-management"
     #   # "allowedValues": [ <array-with-permitted-parameter-values> ],
