a8be7665e3a6923147c05e2d6627d8be21b4d3ea
M. Kozlik <malkak@skyvar.co.il>
2024-01-08 12:48:14 +0200

    remove key vault from terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -93,16 +93,16 @@ data "azurerm_client_config" "current_client" {}
 #   key_vault_id = azurerm_key_vault.key_vault.id
 # }
 
-data "azurerm_key_vault" "key_vault" {
-  name                = var.key_vault_name
-  resource_group_name = var.key_vault_resource_group_name
-}
+# data "azurerm_key_vault" "key_vault" {
+#   name                = var.key_vault_name
+#   resource_group_name = var.key_vault_resource_group_name
+# }
 
-resource "azurerm_key_vault_secret" "key_vault_secret" {
-  name         = var.key_vault_secret_name
-  value        = data.azurerm_storage_account.vnet_storage_account.primary_connection_string
-  key_vault_id = data.azurerm_key_vault.key_vault.id
-}
+# resource "azurerm_key_vault_secret" "key_vault_secret" {
+#   name         = var.key_vault_secret_name
+#   value        = data.azurerm_storage_account.vnet_storage_account.primary_connection_string
+#   key_vault_id = data.azurerm_key_vault.key_vault.id
+# }
 
 resource "azurerm_service_plan" "service_plan" {
   name                = var.app_service_plan_name[count.index]
