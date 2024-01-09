081d240004f741c6d1c8c843bea0fdc683b41330
M. Kozlik <malkak@skyvar.co.il>
2024-01-09 10:09:08 +0200

    add slot development

--- terraform/main.tf
+++ terraform/main.tf
@@ -222,27 +222,27 @@ resource "azurerm_linux_function_app" "linux_function_app" {
   count= length(var.function_app_name)
 }
 
-# resource "azurerm_linux_function_app_slot" "linux_function_app_slot" {
-#   name                 = "development"
-#   function_app_id      = azurerm_linux_function_app.linux_function_app[count.index].id
-#   storage_account_name = data.azurerm_storage_account.vnet_storage_account.name
-#   storage_account_access_key = data.azurerm_storage_account.vnet_storage_account.primary_access_key
+resource "azurerm_linux_function_app_slot" "linux_function_app_slot" {
+  name                 = "development"
+  function_app_id      = azurerm_linux_function_app.linux_function_app[count.index].id
+  storage_account_name = data.azurerm_storage_account.vnet_storage_account.name
+  storage_account_access_key = data.azurerm_storage_account.vnet_storage_account.primary_access_key
 
-#   site_config {
-#     always_on         = true
-#     application_stack {
-#       docker {
-#         registry_url = var.DOCKER_REGISTRY_SERVER_URL
-#         image_name = var.IMAGE_NAME
-#         image_tag = var.IMAGE_TAG
-#         registry_username = var.DOCKER_REGISTRY_SERVER_USERNAME
-#         registry_password = var.DOCKER_REGISTRY_SERVER_PASSWORD
-#       }
-#     }
-#   }
-#   count = length(var.function_app_name)
+  site_config {
+    always_on         = true
+    application_stack {
+      docker {
+        registry_url = var.DOCKER_REGISTRY_SERVER_URL
+        image_name = var.IMAGE_NAME
+        image_tag = var.IMAGE_TAG
+        registry_username = var.DOCKER_REGISTRY_SERVER_USERNAME
+        registry_password = var.DOCKER_REGISTRY_SERVER_PASSWORD
+      }
+    }
+  }
+  count = length(var.function_app_name)
 
-# }
+}
 
 resource "azurerm_logic_app_workflow" "logic_app_workflow" {
   name                = var.logic_app_workflow_name
