a9b2366602682a9b65ce20a7ecc8628d88493a1c
M. Kozlik <malkak@skyvar.co.il>
2024-01-10 10:32:56 +0200

    change to destroy

--- function_apps/func-sending-excel-mark-delete/config_variables.py
+++ function_apps/func-sending-excel-mark-delete/config_variables.py
@@ -12,8 +12,8 @@ client = SecretClient(vault_url=keyvault_uri, credential=credential)
 secret = client.get_secret(secret_name)
 connection_string = secret.value
 
-# excel_connection_string=os.getenv("EXCEL_CONNECTION_STRING")
-# http_trigger_url = os.getenv("HTTP_TRIGGER_URL")
-# deleted_accounts_table=os.getenv("DELETED_ACCOUNTS_TABLE")
-# documentation_table=os.getenv("DOCUMENTATION_TABLE")
-# main_manager=os.getenv("MAIN_MANAGER")
+excel_connection_string=os.getenv("EXCEL_CONNECTION_STRING")
+http_trigger_url = os.getenv("HTTP_TRIGGER_URL")
+deleted_accounts_table=os.getenv("DELETED_ACCOUNTS_TABLE")
+documentation_table=os.getenv("DOCUMENTATION_TABLE")
+main_manager=os.getenv("MAIN_MANAGER")
