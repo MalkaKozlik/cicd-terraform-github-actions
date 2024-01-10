2ff0c01f8e9edaef214398cb713b261cfb0f2ba3
M. Kozlik <malkak@skyvar.co.il>
2024-01-10 14:52:22 +0200

    ang,h

--- function_apps/func-sending-excel-mark-delete/project/managed_deleted_storages.py
+++ function_apps/func-sending-excel-mark-delete/project/managed_deleted_storages.py
@@ -1,9 +1,9 @@
-from azure.data.tables import TableClient
-from azure.core.exceptions import ResourceNotFoundError
+# from azure.data.tables import TableClient
+# from azure.core.exceptions import ResourceNotFoundError
 
 # from config_variables import connection_string, deleted_accounts_table
-import pandas as pd
-import json
+# import pandas as pd
+# import json
 
 
 def deleted_storages(table_name,  test_number, all_storages):
@@ -12,33 +12,33 @@ def deleted_storages(table_name,  test_number, all_storages):
     # delete_storages = check_deleted_storage(all_storages, last_test_storages)
     # upload_deleted_storages_table(connection_string, table_name, delete_storages)
 
-def retrieve_data_from_table(flag,con_str, table_name, query_filter, parameters="None", select=["*"]):
-    try:
-        table = TableClient.from_connection_string(con_str, table_name)
-        queried_entities = table.query_entities(
-            query_filter=query_filter, select=select, parameters=parameters
-        )
-        return convert_to_json(queried_entities) if flag else queried_entities
+# def retrieve_data_from_table(flag,con_str, table_name, query_filter, parameters="None", select=["*"]):
+#     try:
+#         table = TableClient.from_connection_string(con_str, table_name)
+#         queried_entities = table.query_entities(
+#             query_filter=query_filter, select=select, parameters=parameters
+#         )
+#         return convert_to_json(queried_entities) if flag else queried_entities
 
-    except ResourceNotFoundError:
-        raise ResourceNotFoundError("This table does not exist")
-
-
-def convert_to_json(queried_entities):
-    return list((json.loads(pd.Series.to_json(pd.Series(queried_entities)))).values())
+#     except ResourceNotFoundError:
+#         raise ResourceNotFoundError("This table does not exist")
 
 
-def check_deleted_storage(all_storages,last_test_storages):
-    return list(filter(
-        lambda entity: entity["storage_name"] not in all_storages,
-        last_test_storages
-    ))
+# def convert_to_json(queried_entities):
+#     return list((json.loads(pd.Series.to_json(pd.Series(queried_entities)))).values())
 
 
-def upload_deleted_storages_table(conn_str, table_name, delete_storages):
-    for item in delete_storages:
-        parameters = {'storage_name':item["storage_name"], 'PartitionKey': item["PartitionKey"]}
-        deleted_storage_rows =retrieve_data_from_table(False,conn_str, table_name,query_filter="storage_name eq @storage_name and PartitionKey eq @PartitionKey",parameters = parameters,select=["*"] )
+# def check_deleted_storage(all_storages,last_test_storages):
+#     return list(filter(
+#         lambda entity: entity["storage_name"] not in all_storages,
+#         last_test_storages
+#     ))
+
+
+# def upload_deleted_storages_table(conn_str, table_name, delete_storages):
+#     for item in delete_storages:
+#         parameters = {'storage_name':item["storage_name"], 'PartitionKey': item["PartitionKey"]}
+#         deleted_storage_rows =retrieve_data_from_table(False,conn_str, table_name,query_filter="storage_name eq @storage_name and PartitionKey eq @PartitionKey",parameters = parameters,select=["*"] )
         # upload_to_table(deleted_accounts_table, return_the_first(deleted_storage_rows))
 
 
