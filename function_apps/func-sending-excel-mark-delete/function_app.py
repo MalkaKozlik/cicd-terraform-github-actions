9d79941124e824bd2bb969f49128df0770c49f0e
M. Kozlik <malkak@skyvar.co.il>
2024-01-10 15:07:36 +0200

    ang,h

--- function_apps/func-sending-excel-mark-delete/function_app.py
+++ function_apps/func-sending-excel-mark-delete/function_app.py
@@ -2,7 +2,7 @@ import azure.functions as func
 # from project.write_to_excel import *
 # from project.managed_deleted_storages import deleted_storages
 # from config_variables import excel_connection_string, http_trigger_url, main_manager, documentation_table
-from config_variables import excel_connection_string, documentation_table
+# from config_variables import excel_connection_string, documentation_table
 import logging, requests ,json
 
 app = func.FunctionApp()
