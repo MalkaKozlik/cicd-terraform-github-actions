8e21a97f91c63474ded9e1c161dc375cdf8ba2d6
M. Kozlik <malkak@skyvar.co.il>
2024-01-10 16:54:27 +0200

    ang,h

--- function_apps/func-sending-excel-mark-delete/project/managed_deleted_storages.py
+++ function_apps/func-sending-excel-mark-delete/project/managed_deleted_storages.py
@@ -2,7 +2,7 @@ from azure.data.tables import TableClient
 from azure.core.exceptions import ResourceNotFoundError
 
 from config_variables import connection_string, deleted_accounts_table
-# import pandas as pd
+import pandas as pd
 # import json
 
 
