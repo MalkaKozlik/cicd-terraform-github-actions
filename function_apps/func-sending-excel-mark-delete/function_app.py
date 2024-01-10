ac17973dd04369cba6c5175b8ac86740c1887d87
M. Kozlik <malkak@skyvar.co.il>
2024-01-10 16:32:49 +0200

    ang,h

--- function_apps/func-sending-excel-mark-delete/function_app.py
+++ function_apps/func-sending-excel-mark-delete/function_app.py
@@ -1,7 +1,7 @@
 import azure.functions as func
 # from project import write_to_excel, managed_deleted_storages
 # from project.write_to_excel import *
-from project.managed_deleted_storages import deleted_storages
+# from project.managed_deleted_storages import deleted_storages
 from config_variables import excel_connection_string, http_trigger_url, main_manager, documentation_table
 import logging, requests ,json
 
@@ -25,7 +25,7 @@ def test_function(req: func.HttpRequest) -> func.HttpResponse:
         partition_key=data['partition_key']
         all_storages=data['all_storages']
         
-        # write_to_excel.write_to_excel(excel_connection_string, alerts_to_excel)
+        # write_to_excel(excel_connection_string, alerts_to_excel)
         
         # requests.post(
         #     http_trigger_url,
@@ -37,7 +37,7 @@ def test_function(req: func.HttpRequest) -> func.HttpResponse:
         # })
         
 
-        deleted_storages(documentation_table, int(partition_key)-1 , all_storages)
+        # deleted_storages(documentation_table, int(partition_key)-1 , all_storages)
 
     except Exception as e:
         logging.warn(f"-<<->>-{e}")
