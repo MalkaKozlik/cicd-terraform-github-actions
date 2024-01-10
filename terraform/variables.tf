8e61b79c36360e099a0829c316aebc9db49728b3
M. Kozlik <malkak@skyvar.co.il>
2024-01-10 11:42:16 +0200

    change destroy

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -68,30 +68,30 @@ variable key_vault_resource_group_name {
 }
 
 
-# variable key_vault_sku_name {
-#   type        = string
-#   default     = "standard"
-# }
+variable key_vault_sku_name {
+  type        = string
+  default     = "standard"
+}
 
-# variable key_vault_certificate_permissions {
-#   type        = list
-#   default = ["Get", "List", "Update", "Create", "Import", "Delete", "Recover", "Backup", "Restore"]
-# }
+variable key_vault_certificate_permissions {
+  type        = list
+  default = ["Get", "List", "Update", "Create", "Import", "Delete", "Recover", "Backup", "Restore"]
+}
 
-# variable key_vault_key_permissions {
-#   type        = list
-#   default = ["Create","Get"]
-# }
+variable key_vault_key_permissions {
+  type        = list
+  default = ["Create","Get"]
+}
 
-# variable key_vault_secret_permissions {
-#   type        = list
-#   default = ["Get","Set","Delete","Purge","Recover"]
-# }
+variable key_vault_secret_permissions {
+  type        = list
+  default = ["Get","Set","Delete","Purge","Recover"]
+}
 
-# variable key_vault_storage_permissions {
-#   type        = list
-#   default =  ["Get", ]
-# }
+variable key_vault_storage_permissions {
+  type        = list
+  default =  ["Get", ]
+}
 
 # variable key_vault_secret_name {
 #   type        = string
