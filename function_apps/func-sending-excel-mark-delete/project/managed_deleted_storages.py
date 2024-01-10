0f294a937bb0b1ca751d7b821450daaa794e9668
M. Kozlik <malkak@skyvar.co.il>
2024-01-10 14:38:54 +0200

    ang,h

--- function_apps/func-sending-excel-mark-delete/project/managed_deleted_storages.py
+++ function_apps/func-sending-excel-mark-delete/project/managed_deleted_storages.py
@@ -1,16 +1,16 @@
 from azure.data.tables import TableClient
 from azure.core.exceptions import ResourceNotFoundError
 
-from config_variables import connection_string, deleted_accounts_table
+# from config_variables import connection_string, deleted_accounts_table
 import pandas as pd
 import json
 
 
 def deleted_storages(table_name,  test_number, all_storages):
     parameters = {'name': str(test_number)}
-    last_test_storages = retrieve_data_from_table(False, connection_string,table_name,query_filter="PartitionKey eq @name",parameters=parameters, select=["PartitionKey,storage_name"])
-    delete_storages = check_deleted_storage(all_storages, last_test_storages)
-    upload_deleted_storages_table(connection_string, table_name, delete_storages)
+    # last_test_storages = retrieve_data_from_table(False, connection_string,table_name,query_filter="PartitionKey eq @name",parameters=parameters, select=["PartitionKey,storage_name"])
+    # delete_storages = check_deleted_storage(all_storages, last_test_storages)
+    # upload_deleted_storages_table(connection_string, table_name, delete_storages)
 
 def retrieve_data_from_table(flag,con_str, table_name, query_filter, parameters="None", select=["*"]):
     try:
@@ -39,15 +39,15 @@ def upload_deleted_storages_table(conn_str, table_name, delete_storages):
     for item in delete_storages:
         parameters = {'storage_name':item["storage_name"], 'PartitionKey': item["PartitionKey"]}
         deleted_storage_rows =retrieve_data_from_table(False,conn_str, table_name,query_filter="storage_name eq @storage_name and PartitionKey eq @PartitionKey",parameters = parameters,select=["*"] )
-        upload_to_table(deleted_accounts_table, return_the_first(deleted_storage_rows))
+        # upload_to_table(deleted_accounts_table, return_the_first(deleted_storage_rows))
 
 
-def upload_to_table(my_table_name, my_entity):
-    table_client = TableClient.from_connection_string(
-        connection_string, table_name=my_table_name
-    )
-    table_client.create_entity(entity=my_entity)
-    return True
+# def upload_to_table(my_table_name, my_entity):
+#     table_client = TableClient.from_connection_string(
+#         connection_string, table_name=my_table_name
+#     )
+#     table_client.create_entity(entity=my_entity)
+#     return True
 
 
 def return_the_first(objet):
