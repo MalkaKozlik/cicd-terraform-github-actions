1e874da4e8fa6e2fba6044204b565221ee2b99cb
M. Kozlik <malkak@skyvar.co.il>
2024-01-10 11:40:00 +0200

    add access policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -65,38 +65,38 @@ data "azurerm_storage_account" "vnet_storage_account"{
 
 data "azurerm_client_config" "current_client" {}
 
-# resource "azurerm_key_vault" "key_vault" {
-#   name                = var.key_vault_name
-#   location            = "West Europe"
-#   resource_group_name = data.azurerm_storage_account.vnet_storage_account.resource_group_name
-#   soft_delete_retention_days  = 7
-#   tenant_id           = data.azurerm_client_config.current_client.tenant_id
-#   sku_name            = var.key_vault_sku_name
-
-#   access_policy {
-#     tenant_id = data.azurerm_client_config.current_client.tenant_id
-#     object_id = data.azurerm_client_config.current_client.object_id
-
-#     certificate_permissions = var.key_vault_certificate_permissions
-
-#     key_permissions = var.key_vault_key_permissions
-
-#     secret_permissions = var.key_vault_secret_permissions
-
-#     storage_permissions = var.key_vault_storage_permissions
-#   }
-# }
-
-# resource "azurerm_key_vault_secret" "key_vault_secret" {
-#   name         = var.key_vault_secret_name
-#   value        = data.azurerm_storage_account.vnet_storage_account.primary_connection_string
-#   key_vault_id = azurerm_key_vault.key_vault.id
-# }
-
-data "azurerm_key_vault" "key_vault" {
+resource "azurerm_key_vault" "key_vault" {
   name                = var.key_vault_name
-  resource_group_name = var.key_vault_resource_group_name
+  location            = "West Europe"
+  resource_group_name = data.azurerm_storage_account.vnet_storage_account.resource_group_name
+  soft_delete_retention_days  = 7
+  tenant_id           = data.azurerm_client_config.current_client.tenant_id
+  sku_name            = var.key_vault_sku_name
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current_client.tenant_id
+    object_id = data.azurerm_client_config.current_client.object_id
+
+    certificate_permissions = var.key_vault_certificate_permissions
+
+    key_permissions = var.key_vault_key_permissions
+
+    secret_permissions = var.key_vault_secret_permissions
+
+    storage_permissions = var.key_vault_storage_permissions
+  }
 }
+
+resource "azurerm_key_vault_secret" "key_vault_secret" {
+  name         = var.key_vault_secret_name
+  value        = data.azurerm_storage_account.vnet_storage_account.primary_connection_string
+  key_vault_id = azurerm_key_vault.key_vault.id
+}
+
+# data "azurerm_key_vault" "key_vault" {
+#   name                = var.key_vault_name
+#   resource_group_name = var.key_vault_resource_group_name
+# }
 
 # resource "azurerm_key_vault_secret" "key_vault_secret" {
 #   name         = var.key_vault_secret_name
@@ -140,10 +140,10 @@ resource "azurerm_linux_function_app" "linux_function_app" {
     WEBSITE_VNET_ROUTE_ALL=1
 
     DOCUMENTATION_TABLE = "documentation"
-    # SECRET = azurerm_key_vault_secret.key_vault_secret.name
+    SECRET = azurerm_key_vault_secret.key_vault_secret.name
+    KEYVAULT_URI = azurerm_key_vault.key_vault.vault_uri
+    # SECRET = var.key_vault_secret_name
     # KEYVAULT_URI = data.azurerm_key_vault.key_vault.vault_uri
-    SECRET = var.key_vault_secret_name
-    KEYVAULT_URI = data.azurerm_key_vault.key_vault.vault_uri
     https_only                          = true
     DOCKER_REGISTRY_SERVER_URL          = var.DOCKER_REGISTRY_SERVER_URL
     DOCKER_REGISTRY_SERVER_USERNAME     = var.DOCKER_REGISTRY_SERVER_USERNAME
@@ -172,10 +172,10 @@ resource "azurerm_linux_function_app" "linux_function_app" {
     ALERTS_DOCUMENTATION="alertsDocumentation"
     DOCUMENTATION_STORAGE_NAME="myfirsttrail"
 
-    # SECRET = azurerm_key_vault_secret.key_vault_secret.name
+    SECRET = azurerm_key_vault_secret.key_vault_secret.name
+    KEYVAULT_URI = azurerm_key_vault.key_vault.vault_uri
+    # SECRET = var.key_vault_secret_name
     # KEYVAULT_URI = data.azurerm_key_vault.key_vault.vault_uri
-    SECRET = var.key_vault_secret_name
-    KEYVAULT_URI = data.azurerm_key_vault.key_vault.vault_uri
     https_only                          = true
     DOCKER_REGISTRY_SERVER_URL          = var.DOCKER_REGISTRY_SERVER_URL
     DOCKER_REGISTRY_SERVER_USERNAME     = var.DOCKER_REGISTRY_SERVER_USERNAME
@@ -190,10 +190,10 @@ resource "azurerm_linux_function_app" "linux_function_app" {
     DOCUMENTATION_TABLE ="documentation"
     DELETED_ACCOUNTS_TABLE="deletedStorages"
 
-    # SECRET = azurerm_key_vault_secret.key_vault_secret.name
+    SECRET = azurerm_key_vault_secret.key_vault_secret.name
+    KEYVAULT_URI = azurerm_key_vault.key_vault.vault_uri
+    # SECRET = var.key_vault_secret_name
     # KEYVAULT_URI = data.azurerm_key_vault.key_vault.vault_uri
-    SECRET = var.key_vault_secret_name
-    KEYVAULT_URI = data.azurerm_key_vault.key_vault.vault_uri
     https_only                          = true
     DOCKER_REGISTRY_SERVER_URL          = var.DOCKER_REGISTRY_SERVER_URL
     DOCKER_REGISTRY_SERVER_USERNAME     = var.DOCKER_REGISTRY_SERVER_USERNAME
@@ -218,6 +218,17 @@ resource "azurerm_linux_function_app" "linux_function_app" {
     type = "SystemAssigned"
   }
   count= length(var.function_app_name)
+}
+
+resource "azurerm_key_vault_access_policy" "example-principal" {
+  key_vault_id = azurerm_key_vault.key_vault.id
+  tenant_id    = data.azurerm_client_config.current_client.tenant_id
+  object_id  = azurerm_function_app.function_app[count.index].identity[0].principal_id
+
+  key_permissions = [
+    "Get", "List", "Encrypt", "Decrypt"
+  ]
+  count = length(var.function_app_name)
 }
 
 # resource "azurerm_linux_function_app_slot" "linux_function_app_slot" {
