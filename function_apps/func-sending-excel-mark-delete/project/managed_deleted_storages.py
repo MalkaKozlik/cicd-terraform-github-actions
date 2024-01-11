d2c68d14784a1acf0d097d1c43e5132478d8c73b
M. Kozlik <malkak@skyvar.co.il>
2024-01-11 13:10:58 +0200

    add

--- function_apps/func-sending-excel-mark-delete/project/managed_deleted_storages.py
+++ function_apps/func-sending-excel-mark-delete/project/managed_deleted_storages.py
@@ -4,7 +4,6 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-
 from config_variables import connection_string, deleted_accounts_table
 import json, pandas as pd
 
