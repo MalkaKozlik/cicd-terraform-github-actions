6bd23a3198bd4647342faa7be8d2337400c13beb
M. Kozlik <malkak@skyvar.co.il>
2024-01-16 14:30:16 +0200

    add

--- function_apps/func-send-excel-mark-delete/function_app.py
+++ function_apps/func-send-excel-mark-delete/function_app.py
@@ -5,11 +5,11 @@ import requests
 import json
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from project.write_to_excel import write_to_excel
-from project.managed_deleted_storages import deleted_storages
-from project.config_variables import *
+# from project.write_to_excel import write_to_excel
+# from project.managed_deleted_storages import deleted_storages
+# from project.config_variables import *
 
 
 app = func.FunctionApp()
@@ -28,23 +28,23 @@ def test_function(req: func.HttpRequest) -> func.HttpResponse:
         my_json = body.decode('utf8').replace("'", '"')
         data = json.loads(my_json)
         
-        alerts_to_excel=data['alerts_to_excel']
-        partition_key=data['partition_key']
-        all_storages=data['all_storages']
+        # alerts_to_excel=data['alerts_to_excel']
+        # partition_key=data['partition_key']
+        # all_storages=data['all_storages']
         
-        write_to_excel(excel_connection_string, alerts_to_excel)
+        # write_to_excel(excel_connection_string, alerts_to_excel)
         
-        requests.post(
-            http_trigger_url,
-            json={
-                "recipient_email": main_manager,
-                "subject": "Summary Alerts For Storage Accounts",
-                "body": "summary file",
-                "excel":"alert_file.xlsx"
-        })
+        # requests.post(
+        #     http_trigger_url,
+        #     json={
+        #         "recipient_email": main_manager,
+        #         "subject": "Summary Alerts For Storage Accounts",
+        #         "body": "summary file",
+        #         "excel":"alert_file.xlsx"
+        # })
         
 
-        deleted_storages(documentation_table, int(partition_key)-1 , all_storages)
+        # deleted_storages(documentation_table, int(partition_key)-1 , all_storages)
 
     except Exception as e:
         logging.warn(f"-<<->>-{e}")
