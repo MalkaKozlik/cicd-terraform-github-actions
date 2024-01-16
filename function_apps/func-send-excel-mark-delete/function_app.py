38cdf74c6bb84ba93785d5a3141029b14075de2b
M. Kozlik <malkak@skyvar.co.il>
2024-01-16 15:48:06 +0200

    add

--- function_apps/func-send-excel-mark-delete/function_app.py
+++ function_apps/func-send-excel-mark-delete/function_app.py
@@ -25,12 +25,12 @@ def test_function(req: func.HttpRequest) -> func.HttpResponse:
     try:
         
         body = req.get_body()
-        # my_json = body.decode('utf8').replace("'", '"')
-        # data = json.loads(my_json)
+        my_json = body.decode('utf8').replace("'", '"')
+        data = json.loads(my_json)
         
-        # alerts_to_excel=data['alerts_to_excel']
-        # partition_key=data['partition_key']
-        # all_storages=data['all_storages']
+        alerts_to_excel=data['alerts_to_excel']
+        partition_key=data['partition_key']
+        all_storages=data['all_storages']
 
         # result = write_to_excel(excel_connection_string, alerts_to_excel)
         # logging.info(result)
