73d49e1a76467a526d4fef2f4e2032fcc7ea793f
M. Kozlik <malkak@skyvar.co.il>
2024-01-17 13:20:51 +0200

    add

--- function_apps/func-send-excel-mark-delete/project/try_excel.py
+++ function_apps/func-send-excel-mark-delete/project/try_excel.py
@@ -13,6 +13,6 @@ def create(connection_string,data):
     df.to_excel('alert_file.xlsx', index=False, header=custom_header)
     # df.to_excel('output.xlsx', index=False, na_rep='N/A', header=custom_header, index_label='ID')
 
-    # blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-    # blob_client = blob_service_client.get_blob_client(container='excel', blob='alert_file.xlsx')
-    # blob_client.upload_blob(df.to_excel('alert_file.xlsx', index=False, header=custom_header), overwrite=True)
+    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+    blob_client = blob_service_client.get_blob_client(container='excel', blob='alert_file.xlsx')
+    blob_client.upload_blob(df.to_excel('alert_file.xlsx', index=False, header=custom_header), overwrite=True)
