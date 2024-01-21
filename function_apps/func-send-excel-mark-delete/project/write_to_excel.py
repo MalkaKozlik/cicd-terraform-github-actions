2d83edd0ad685ac2a8491fffdfa3ad7a143ecda2
M. Kozlik <malkak@skyvar.co.il>
2024-01-21 12:48:44 +0200

    change write_to_excel

--- function_apps/func-send-excel-mark-delete/project/write_to_excel.py
+++ function_apps/func-send-excel-mark-delete/project/write_to_excel.py
@@ -1,21 +1,25 @@
 from azure.storage.blob import BlobServiceClient
 import pandas as pd
 import os
-import logging
+
 
 def write_and_upload(connection_string, data):
 
+    blob_file='alert_file.xlsx'
+    container='excel'
+    write_excel_file(data, blob_file)
+    upload_blob_file(connection_string, container, blob_file)
+
+
+def write_excel_file(data, blob_file):
     df = pd.DataFrame(data)
     custom_header = ['Storage Account', 'Alert Body','Subscription Name','Subscription Manager Email']
-    df.to_excel('alert_file.xlsx', index=False, header=custom_header)
-    
-    upload_blob_file(connection_string)
+    df.to_excel(blob_file, index=False, header=custom_header)
 
 
-def upload_blob_file(connection_string):
+def upload_blob_file(connection_string, container, blob_file):
 
     blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-    container_client = blob_service_client.get_container_client(container='excel')
-    with open(file=os.path.join('./', 'alert_file.xlsx'), mode="rb") as data:
-        blob_client = container_client.upload_blob(name='alert_file.xlsx', data=data, overwrite=True)
-        logging.warn(f'blob client: {blob_client}')
+    container_client = blob_service_client.get_container_client(container=container)
+    with open(os.path.join('./', blob_file), "rb") as data:
+        container_client.upload_blob(name=blob_file, data=data, overwrite=True)
