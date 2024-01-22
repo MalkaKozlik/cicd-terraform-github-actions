223b97370d9a256c1def0a3d437276417f80a7bb
M. Kozlik <malkak@skyvar.co.il>
2024-01-22 11:28:07 +0200

    push

--- services/storage_account/func-get-storage-list-by-subscription/function_app.py
+++ services/storage_account/func-get-storage-list-by-subscription/function_app.py
@@ -9,7 +9,7 @@ app = func.FunctionApp()
 @app.function_name(name="HttpTrigger1")
 @app.route(route="")
 def test_function(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request!!!!!!3')
+    logging.info('Python HTTP trigger function processed a request!!!!!!4')
 
     try: 
         fix_bytes_value = req.get_body().replace(b"'", b'"')
