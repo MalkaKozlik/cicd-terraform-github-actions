ca88f18316b4f769f7a76436cf37045b813e95d5
M. Kozlik <malkak@skyvar.co.il>
2024-01-23 16:09:18 +0200

    stam

--- function_apps/func-get-subscription-list/function_app.py
+++ function_apps/func-get-subscription-list/function_app.py
@@ -14,9 +14,9 @@ def test_function(req: func.HttpRequest) -> func.HttpResponse:
 
     try:
         subscription_list = get_subscription_list()
-        
+        logging.info(f"-----------------------{subscription_list}")
         partition_key = str(get_a_last_partitionKey_number(documentation_table) + 1)
-
+        logging.info(f"---++++++++++++++++++--{partition_key}")
         subscriptions=[]
         for subscription in subscription_list:
             subscriptions.append({'subscription_id':subscription.subscription_id,'subscription_name':subscription.display_name})
@@ -25,7 +25,7 @@ def test_function(req: func.HttpRequest) -> func.HttpResponse:
         logging.warn(f"---{e}")
 
     answer={'subscriptions':subscriptions,'partition_key':str(partition_key)}
-
+    logging.warn("88888888888888888888888888888888888888888888888888")
     logging.info(answer)
 
     return func.HttpResponse(str(answer), status_code=200)
