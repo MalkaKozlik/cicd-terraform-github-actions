e7c126d7ed383405a58f9f147c0aca78c9a9008d
M. Kozlik <malkak@skyvar.co.il>
2024-01-23 10:36:25 +0200

    add hub

--- terraform-hub/main.tf
+++ terraform-hub/main.tf
@@ -27,28 +27,28 @@ resource "azurerm_resource_group" "resource_group" {
 resource "azurerm_virtual_network" "virtual_network" {
   name                = var.virtual_networks_names[count.index]
   address_space       = var.virtual_networks_address_space[count.index]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
   count = length(var.virtual_networks_names)
 }
 
 resource "azurerm_virtual_wan" "virtual_wan" {
   name                = "vwan"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  location            = azurerm_resource_group.resource_group.location
 }
 
 resource "azurerm_virtual_hub" "virtual_hub" {
   name                = "virtual-hub"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  virtual_wan_id      = azurerm_virtual_wan.example.id
+  resource_group_name = azurerm_resource_group.resource_group.name
+  location            = azurerm_resource_group.resource_group.location
+  virtual_wan_id      = azurerm_virtual_wan.virtual_wan.id
   address_prefix      = "10.0.1.0/24"
 }
 
 resource "azurerm_virtual_hub_connection" "virtual_hub_connection" {
   name                      = var.virtual_hub_connection_names[count.index]
-  virtual_hub_id            = azurerm_virtual_hub.example.id
-  remote_virtual_network_id = azurerm_virtual_network.example.id
+  virtual_hub_id            = azurerm_virtual_hub.virtual_hub[count.index].id
+  remote_virtual_network_id = azurerm_virtual_network.virtual_network[count.index].id
   count= length(var.virtual_hub_connection_names)
 }
