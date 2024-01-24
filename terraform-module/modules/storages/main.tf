e3d97fee901de1b5d4ecc6888739d061009e6a29
M. Kozlik <malkak@skyvar.co.il>
2024-01-24 13:18:54 +0200

    add terraform-module

---
+++ terraform-module/modules/storages/main.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "resource"{
+  name     = "terraform-module-try"
+  location = "West Europe"
+}
---
+++ terraform-module/project/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name      = "NetworkWatcherRG"
+    storage_account_name     = "myfirsttrail"
+    container_name           = "terraform-modules-demo"
+    key                      = "terraform.tfstate"
+  }
+  
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+
+  subscription_id = var.subscription_id
+}
+
+
+module storages{
+    source = "../modules/storages"
+}
