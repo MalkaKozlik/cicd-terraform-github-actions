3260afb3c465f2138c5f2c9d2e522d2b112b35b0
M. Kozlik <malkak@skyvar.co.il>
2024-02-05 15:26:06 +0200

    delete pages that not used

--- terraform-module/modules/storages/main.tf
+++
@@ -1,9 +0,0 @@
-resource "azurerm_resource_group" "resource"{
-  name     = var.resource_group_name
-  location = var.resource_group_location
-}
-
-output id {
-  value       = azurerm_resource_group.resource.id
-}
-
