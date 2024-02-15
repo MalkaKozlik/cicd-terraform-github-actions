6ad6f4dfdb1f1ef29e60ae772c4498a219330e36
B. Gliksberg <batyag@skyvar.co.il>
2024-02-15 16:23:20 +0200

    loggic-app

--- function_apps/func-three-01/project/read_log_analytics.py
+++ function_apps/func-three-01/project/read_log_analytics.py
@@ -6,10 +6,8 @@ from datetime import timedelta
 from itertools import groupby
 from operator import itemgetter
 
-import logging
 
 def get_array_of_last_fetch_time():
-    logging.warn("----start----")
     times=get_times()
     max_time_foreach_storage=get_max_times_log_for_each_storage(times)
     
@@ -22,9 +20,7 @@ def get_times():
     times=[]
 
     for type in storage_types:
-        logging.warn(type)
         data_for_type_query = get_workspace_table(type)
-        logging.warn(data_for_type_query)
         times_array = get_times_logs(data_for_type_query)
         times.extend(times_array)
 
