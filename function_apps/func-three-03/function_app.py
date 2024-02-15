7e473443f31f0bd4a00039889701db9d37944353
B. Gliksberg <batyag@skyvar.co.il>
2024-02-15 14:36:21 +0200

    deleted 1

--- function_apps/func-three-03/function_app.py
+++ function_apps/func-three-03/function_app.py
@@ -4,7 +4,7 @@ import io
 
 from project.managed_storages import *
 from config.config_variables import essential_tag
-
+import logging
 
 app = func.FunctionApp()
 
@@ -17,6 +17,8 @@ def func_three_03(req: func.HttpRequest) -> func.HttpResponse:
     storage_account_list = get_storage_list(subscriptions_json['subscription_id'])
     storage_accounts=[]
     for storage_account in storage_account_list:
+        logging.info('true' if storage_account.tags.get(essential_tag) else 'false')
         storage_accounts.append({'name':storage_account.name,'id':storage_account.id,'tags':'true' if storage_account.tags.get(essential_tag) else 'false'})
 
+    logging.warn(storage_accounts)
     return func.HttpResponse(str(storage_accounts), status_code=200)
