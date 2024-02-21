53b2fa3f5afee9cfffbe7570eb05ecb752432905
B. Gliksberg <batyag@skyvar.co.il>
2024-02-21 09:25:45 +0200

    user disable

---
+++ function_apps/user-disable-automation/project/config_variables.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+tenant_id = os.getenv("TENANT_ID")
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
+application_id = os.getenv("APPLICATION_ID")
+keyvault_uri = os.getenv("KEYVAULT_URI")
