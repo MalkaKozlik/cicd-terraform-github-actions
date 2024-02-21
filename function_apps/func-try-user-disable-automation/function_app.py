0402257ce73c97ec1e1a8e7ea9541ebe694557b6
B. Gliksberg <batyag@skyvar.co.il>
2024-02-21 09:45:34 +0200

    wow

--- function_apps/func-try-user-disable-automation/function_app.py
+++ function_apps/func-try-user-disable-automation/function_app.py
@@ -1,17 +1,17 @@
 import azure.functions as func
 import logging
 
-from project.process_management import inspection_process_management
+# from project.process_management import inspection_process_management
 
 app = func.FunctionApp()
 
 
 @app.function_name(name="HttpTrigger1")
 @app.route(route="")
-def func_three_05(req: func.HttpRequest) -> func.HttpResponse:
+def test_function(req: func.HttpRequest) -> func.HttpResponse:
 
     logging.info('Python HTTP trigger function processed a request.')
 
-    inspection_process_management("Moon","3")
+    # inspection_process_management("Moon","3")
         
     return func.HttpResponse("success - end logic app", status_code=200)
