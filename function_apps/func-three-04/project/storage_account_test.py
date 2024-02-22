4b551b9fdc620a7de068a1542b8348209b8a7af1
B. Gliksberg <batyag@skyvar.co.il>
2024-02-22 15:47:20 +0200

    wow

--- function_apps/func-three-04/project/storage_account_test.py
+++ function_apps/func-three-04/project/storage_account_test.py
@@ -11,13 +11,13 @@ from project.used_capacity_comparison import *
 def storage_account_test(storage_account,partitionKey,row_key,subscription_id,subscription_name,storage_account_id,last_fetch_time):
     logging.warn('in storage_account_test')
     storage_client = create_storage_management_client(subscription_id)
-    logging.warn(f"storage_client: {storage_client}")
+    # logging.warn(f"storage_client: {storage_client}")
     resource_group_name = find_resource_group_name(storage_account_id)
-    logging.warn(f"resource_group_name: {resource_group_name}")
+    # logging.warn(f"resource_group_name: {resource_group_name}")
     used_capacity_comparison_test_result = used_capacity_comparison_test(resource_group_name, storage_account, subscription_id)
-    logging.warn(f"used_capacity_comparison_test_result: {used_capacity_comparison_test_result}")
+    # logging.warn(f"used_capacity_comparison_test_result: {used_capacity_comparison_test_result}")
     last_fetch_is_early_result = check_last_fetch_is_early(storage_client,used_capacity_comparison_test_result["resource_group"],storage_account,last_fetch_time)
-    logging.warn(f"last_fetch_is_early_result: {last_fetch_is_early_result}")
+    # logging.warn(f"last_fetch_is_early_result: {last_fetch_is_early_result}")
 
     alert_reason_for_check_used_capacity = (
         alert_reasons.USED_CAPACITY.value
@@ -29,7 +29,7 @@ def storage_account_test(storage_account,partitionKey,row_key,subscription_id,su
         if last_fetch_is_early_result["alert"]
         else "null"
     )
-    logging.info(f"{alert_reason_for_check_used_capacity} - {alert_reason_for_check_last_fetch}")
+    logging.info(f"creating")
     entity = creating_an_object_for_sending_to_a_documentation_table(
         str(partitionKey),
         str(row_key),
@@ -129,4 +129,6 @@ def creating_an_object_for_sending_to_a_documentation_table(
     }
     if last_storage_fetch_time:
         obj["last_storage_fetch_time"] = last_storage_fetch_time
+
+    logging.warning(f"obj!!!:  {obj}")
     return obj
