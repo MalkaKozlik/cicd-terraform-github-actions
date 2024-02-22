c01c140340b433339c5e97bc7256c2ba740e8e62
B. Gliksberg <batyag@skyvar.co.il>
2024-02-22 15:14:38 +0200

    wow

--- function_apps/func-three-04/project/connect_to_azure.py
+++ function_apps/func-three-04/project/connect_to_azure.py
@@ -5,6 +5,7 @@ from azure.mgmt.storage import StorageManagementClient
 from azure.mgmt.monitor import MonitorManagementClient
 import pandas as pd
 import json
+import logging
 
 from config.config_variables import connection_string
 
@@ -56,5 +57,7 @@ def upload_to_table(my_table_name, my_entity):
     table_client = TableClient.from_connection_string(
         connection_string, table_name=my_table_name
     )
+    logging.info('connection_string')
+    logging.warn(connection_string)
     table_client.create_entity(entity=my_entity)
     return True
